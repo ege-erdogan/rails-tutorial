b77920a7facbacfeefc4d2f5628247d3880f0330
egeerdogan <ege@egeerdogan.com>
2019-02-06 11:11:28 +0300

    initial commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user # this is converted to user_url(user)
+    else
+      # Unlike the contents of flash, the contents of flash.now disappear as soon as
+      # there is an additional request
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,23 @@
+module SessionsHelper
+
+  def log_in(user)
+    # Because temp. cookies created this way are encrypted, this code is safe.
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+end
